9ee9564237681bc7b1f7059c1b5c6c3cbee2a0b7
keith <keith@raincityambience.com>
2016-11-11 00:22:00 -0700

    create basic styling for game

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#user-btn-hit").on("click", function(){
+    $(".card1").addClass("move");
+    $(".card2").addClass("move");
+    $(".hit-card1-test").addClass("show");
+    // $(".hit-card2-test").addClass("hidden");
+  })
+
+
+})
