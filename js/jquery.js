55af76e71142a8301e90d6889bee13fe7b5bcf3f
keith <keith@raincityambience.com>
2016-11-12 18:25:36 -0700

    add styling tweaks and app.js

--- js/jquery.js
+++ js/jquery.js
@@ -12,49 +12,51 @@ $(document).ready(function() {
 
 //2nd click - 2nd hit card appears
 
-$("#user-btn-hit").one("click",function() {
-    $(this).click(function() {
-       $(".hit-card2-test").addClass("show");
-      //  $(".hit-card1-test").css("margin-left", "-=10px");
-          // $(".hit-card1-test").animate({left: "-=20"}, 1000)
-    });
-});
+  $("#user-btn-hit").one("click",function() {
+      $(this).click(function() {
+         $(".hit-card2-test").addClass("show");
+        //  $(".hit-card1-test").css("margin-left", "-=10px");
+            // $(".hit-card1-test").animate({left: "-=20"}, 1000)
+      });
+  });
 
 //3rd click - 3rd hit card appears
 
-$("#user-btn-hit").one("click",function() {
-    $(this).one("click", function(){
-      $(this).click(function() {
-         $(".hit-card3-test").addClass("show");
-        //  $(".hit-card1-test").animate({left: "-=20"}, 1000)
-        //  $(".hit-card2-test").animate({left: "-=20"}, 1000)
-      });
-    })
-});
-
-
-$("#user-btn-hit").one("click",function() {
-    $(this).one("click", function(){
+  $("#user-btn-hit").one("click",function() {
       $(this).one("click", function(){
         $(this).click(function() {
-           $(".hit-card4-test").addClass("show");
+           $(".hit-card3-test").addClass("show");
+          //  $(".hit-card1-test").animate({left: "-=20"}, 1000)
+          //  $(".hit-card2-test").animate({left: "-=20"}, 1000)
         });
       })
-    })
-});
+  });
 
+//4th click - 4th hit card appears
 
-$("#user-btn-hit").one("click",function() {
-    $(this).one("click", function(){
+  $("#user-btn-hit").one("click",function() {
       $(this).one("click", function(){
         $(this).one("click", function(){
           $(this).click(function() {
-             $(".hit-card5-test").addClass("show");
+             $(".hit-card4-test").addClass("show");
           });
         })
       })
-    })
-});
+  });
+
+//5th click - 5th hit card appears
+
+  $("#user-btn-hit").one("click",function() {
+      $(this).one("click", function(){
+        $(this).one("click", function(){
+          $(this).one("click", function(){
+            $(this).click(function() {
+               $(".hit-card5-test").addClass("show");
+            });
+          })
+        })
+      })
+  });
 
 //reset scores to 0
 
